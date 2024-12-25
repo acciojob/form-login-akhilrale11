65561d7267224a2a13ddf31c6c6aeb4eb2b5d04d
akhilrale11 <117888290+akhilrale11@users.noreply.github.com>
2024-12-25 20:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const codes = document.querySelectorAll('.code');
 
+// Alert functionality for form submission
+const handleFormSubmission = () => {
+  const firstName = document.getElementById('code-1').value;
+  const lastName = document.getElementById('code-2').value;
+  alert(`${firstName} ${lastName}`);
+};
+
+// Attach an event listener to the last input field
+codes[codes.length - 1].addEventListener('input', handleFormSubmission);
+
+// Navigation logic for OTP inputs
 codes.forEach((code, idx) => {
   code.addEventListener('input', (e) => {
     const nextInput = codes[idx + 1];
