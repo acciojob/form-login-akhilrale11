40410374fe2a2208a8b641083e79785ff5ec520b
akhilrale11 <117888290+akhilrale11@users.noreply.github.com>
2024-12-25 20:14:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function getFormvalue() {
-    //Write your code here
+const codes = document.querySelectorAll('.code');
 
-}
+codes.forEach((code, idx) => {
+  code.addEventListener('input', (e) => {
+    const nextInput = codes[idx + 1];
+    if (e.target.value && nextInput) {
+      nextInput.focus();
+    }
+  });
+
+  code.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (!e.target.value && idx > 0) {
+        const prevInput = codes[idx - 1];
+        prevInput.focus();
+      }
+    }
+  });
+
+  // Automatically focus on the first input field when the page loads
+  if (idx === 0) {
+    code.focus();
+  }
+});
